57464f4e320838a9c7303dae35e8ece34a99502a
cercil0605 <kuamaichi2223@gmail.com>
2025-04-29 19:49:08 +0900

    add: connection with DB

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,22 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"self-management-bot/db"
 	"self-management-bot/handler"
 )
 
 func main() {
+	// env read
 	err := godotenv.Load(".env") // 相対パスに注意
 	if err != nil {
 		log.Fatal("❌ Error loading .env file")
 	}
 	token := os.Getenv("DISCORD_BOT_TOKEN")
-
+	// DB Connection
+	if err := db.Init(); err != nil {
+		panic(err)
+	}
+	// session with discord
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal("❌ Error creating Discord session,", err)
--- db/connection.go
+++ db/connection.go
@@ -1,3 +1,19 @@
 package db
 
-// 仮ファイル（PostgreSQL接続用）
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+var DB *sqlx.DB
+
+// Init connection
+func Init() error {
+	var err error
+	DB, err = sqlx.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		return err
+	}
+	return DB.Ping()
+}
