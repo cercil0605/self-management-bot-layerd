c6fdada904870bfff9cb4f7e031740098f3df0d3
cercil0605 <kuamaichi2223@gmail.com>
2025-05-06 23:29:48 +0900

    fixed: booting time shorter

--- client/docker.go
+++ client/docker.go
@@ -1,6 +1,28 @@
 package client
 
-import "os/exec"
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+)
+
+// Dockerコンテナが起動しているか確認
+func IsDockerPostgresRunning() bool {
+	cmd := exec.Command("docker", "ps", "--filter", "name=local-postgres", "--format", "{{.Names}}")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return false
+	}
+	containers := strings.Split(out.String(), "\n")
+	for _, name := range containers {
+		if name == "local-postgres" {
+			return true
+		}
+	}
+	return false
+}
 
 var DockerCmd *exec.Cmd
 
